662e50fcf876cab09465b51fcf5945ee081a1d47
Brenda1339 <81568408+Brenda1339@users.noreply.github.com>
2021-04-08 19:59:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
     return "WELCOME PROJECT-NOVA- "
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+        
+         if port == 5000:
+        app.debug = True
+    
+   app.run(host='0.0.0.0', port=port)
