e464135ca5c66366ad4696dc4b17709b445237bb
brenda <fabiolaesp123g@gmail.com>
2021-06-07 07:36:50 -0400

    [master] se adiciono la clase de conexion a la base de datos

--- main.py
+++ main.py
@@ -7,13 +7,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "WELCOME PROJECT-NOVA- "
-
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-        
-         if port == 5000:
-        app.debug = True
-    
-   app.run(host='0.0.0.0', port=port)
+    return "WELCOME PROJECT-NOVA- "   
