69595097bdf78259f6765a98adbc5554dbba9d6b
brenda <fabiolaesp123g@gmail.com>
2021-06-18 20:39:32 -0400

    add connect server

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, jsonify, abort
+from flask.wrappers import Request
+from models.users import Users
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "WELCOME PROJECT-NOVA- "   
+    return "BIENVENIDO A PROYECTO - NOVA "
+
+@app.route('/PROYECTO-NOVA-/user.browse/<int:id_user>', methods=['GET'])
+def loggin_user(id_user):
+    user = Users(Request.json)
+    user_id = user.browse_user(id_user)
+    return jsonify({'user_id': user_id}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- models/users.py
+++ models/users.py
@@ -4,13 +4,14 @@ from data_base import DataBase, db
 class Users:
     def __init__(self, users):
 
+        self._id_user = users.get('id_user', False)
         self._name = users.get('name', False)
         self._password = users.get('password', False)
     
     @staticmethod
     def browse_user(id_user):
-        browse_user_query= """ select * from users where id_user= 
-        {id_user}""".format(id_user=id_user)
+        browse_user_query= """ select * from users where 
+        id_user= {id_user}""".format(id_user=id_user)
         db = DataBase()
         ps_connection = db.session()
         ps_cursor = ps_connection.cursor() 
@@ -19,6 +20,11 @@ class Users:
         ps_cursor.close()
         return user
     
+
+    @property
+    def id_user(self):
+        return self._id_user
+
     @property
     def name(self):
         return self._name
@@ -26,7 +32,11 @@ class Users:
     @property
     def password(self):
         return self._password
-    
+
+    @id_user.setter
+    def id_user(self, id_user):
+        self._id_user = id_user
+
     @name.setter
     def name(self, name):
         self._name = name
